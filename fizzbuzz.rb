41f8ae06d5d237c4b792654268759cf45787331b
Harriet Craven <harrietcraven@Harriets-MacBook-Pro.local>
2015-08-31 21:37:15 +0100

    Updated fizzbuzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,17 @@
-# fizzbuzz
+def fizzbuzz(n)
+	return 'FizzBuzz' if is_divisible_by_fifteen?(n)
+	return 'Fizz' if is_divisible_by_five?(n)
+	return 'Buzz' if is_divisible_by_three?(n)
+end
+
+def is_divisible_by_three?(n)
+	n % 3 == 0
+end
+
+def is_divisible_by_five?(n)
+	n % 5 == 0
+end
+
+def is_divisible_by_fifteen?(n)
+	n % 15== 0
+end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,1 +1,26 @@
-# spec
+# specification for fizzbuzz
+
+require_relative '../fizzbuzz.rb'
+
+describe 'fizzbuzz' do
+  context 'when given a number n' do
+    it 'can check if 3 divides n' do
+      expect(divides?(3, 30)).to be true
+      expect(divides?(3, 31)).to be false
+    end
+    it 'can check if 5 divides n' do
+      expect(divides?(5, 30)).to be true
+      expect(divides?(5, 31)).to be false
+    end
+    it 'can check if 15 divides n' do
+      expect(divides?(15, 30)).to be true
+      expect(divides?(15, 31)).to be false
+    end
+    it 'can convert to fizz, buzz or fizzbuzz' do
+      expect(fizzbuzzify(7)).to eq 7
+      expect(fizzbuzzify(9)).to eq 'fizz'
+      expect(fizzbuzzify(10)).to eq 'buzz'
+      expect(fizzbuzzify(30)).to eq 'fizzbuzz'
+    end
+  end
+end
